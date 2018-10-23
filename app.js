fa150ad65d28e4e8676e7696bc84e0aa511d77ec
Michael Hanley <mhanley@sacfoodcoop.com>
2018-10-22 19:51:08 -0700

    Add Heroku port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const hostname = 'localhost';
-const port = 3100;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
