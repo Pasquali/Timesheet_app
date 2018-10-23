f347257141c775a6df344005f4be7c354aaa6c4f
Michael Hanley <mhanley@sacfoodcoop.com>
2018-10-22 20:53:54 -0700

    update app.js

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('./controllers'));
 
-app.listen(port, hostname, function() {
+app.listen(port, () => {
   console.log('Listening on port 3100...')
 });
