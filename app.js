6e12246cd47cab02e881d4daff705370d8537793
Michael Hanley <mhanley@sacfoodcoop.com>
2018-10-22 21:40:49 -0700

    update

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3100;
 const bodyParser = require('body-parser');
+const path = require("path")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('./controllers'));
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log('Listening on port 3100...')
